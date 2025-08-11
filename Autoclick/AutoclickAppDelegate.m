323d7f90fb7b52f4d7d20774fb570340c0f198f1
natnpalen <nathanielapalencia@gmail.com>
2025-08-11 13:56:27 -0700

    Add interval mode (click every X minutes)

--- Autoclick/AutoclickAppDelegate.m
+++ Autoclick/AutoclickAppDelegate.m
@@ -5,6 +5,27 @@
 
 #import "AutoclickAppDelegate.h"
 @import IOKit;
+#import <ApplicationServices/ApplicationServices.h>
+
+// Private extension so we don't need to edit the .h file
+@interface AutoclickAppDelegate ()
+@property (nonatomic, assign) BOOL ac_intervalModeEnabled;
+@property (nonatomic, assign) NSInteger ac_intervalSeconds;
+@property (nullable, nonatomic, strong) dispatch_source_t ac_intervalTimer;
+
+// Optional UI (connect later in Xcode if you want on-screen controls)
+@property (weak) IBOutlet NSButton *ac_intervalModeCheckbox;
+@property (weak) IBOutlet NSTextField *ac_intervalMinutesField;
+
+// Actions (connect later if you add UI)
+- (IBAction)ac_toggleIntervalMode:(id)sender;
+- (IBAction)ac_intervalMinutesChanged:(id)sender;
+
+- (void)ac_updateUIForIntervalMode;
+- (void)ac_startIntervalTimer;
+- (void)ac_stopIntervalTimer;
+- (void)ac_performSingleClick;
+@end
 
 @implementation NSApplication (AppDelegate)
 
@@ -112,6 +133,21 @@
     }
     
     [window setDelegate:(id<NSWindowDelegate>)self];
+
+    // --- Interval mode defaults & UI reflection ---
+    self.ac_intervalModeEnabled = [userDefaults boolForKey:@"ACIntervalModeEnabled"];
+    NSInteger s = (NSInteger)[userDefaults integerForKey:@"ACIntervalSeconds"];
+    if (s <= 0) s = 60; // default: 1 minute
+    self.ac_intervalSeconds = s;
+
+    if (self.ac_intervalModeCheckbox) {
+        self.ac_intervalModeCheckbox.state = self.ac_intervalModeEnabled ? NSControlStateValueOn : NSControlStateValueOff;
+    }
+    if (self.ac_intervalMinutesField) {
+        NSInteger minutes = MAX(1, (NSInteger)llround((double)self.ac_intervalSeconds / 60.0));
+        self.ac_intervalMinutesField.integerValue = minutes;
+    }
+    [self ac_updateUIForIntervalMode];
 }
 
 - (void)windowWillClose:(NSNotification*)note {
@@ -191,67 +227,79 @@
 }
 
 - (IBAction)startStop:(id)sender {
-    if ([clicker isClicking])
-    {
-        [clicker stopClicking];
+    // If we're currently clicking OR running interval mode, stop everything.
+    if ([clicker isClicking] || self.ac_intervalTimer) {
+        if ([clicker isClicking]) {
+            [clicker stopClicking];
+        }
+        if (self.ac_intervalTimer) {
+            [self ac_stopIntervalTimer];
+        }
+        [self stoppedClicking];
+        return;
     }
-    else
-    {
-        NSDictionary *options = @{(__bridge id) kAXTrustedCheckOptionPrompt : @YES};
-        BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef) options);
 
-        if (!accessibilityEnabled) {
-            // Do not enable clicking if accessibility is off because the user might open the Privacy > Accessibility
-            // settings then check the box next to Autoclick which will immediately be unchecked by the automatic
-            // clicking.
+    NSDictionary *options = @{(__bridge id) kAXTrustedCheckOptionPrompt : @YES};
+    BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef) options);
+
+    if (!accessibilityEnabled) {
+        // Do not enable clicking if accessibility is off because the user might open the Privacy > Accessibility
+        // settings then check the box next to Autoclick which will immediately be unchecked by the automatic
+        // clicking.
+        return;
+    }
+
+    if (@available(macOS 10.15, *)) {
+        BOOL inputMonitoringEnabled = IOHIDCheckAccess(kIOHIDRequestTypeListenEvent) == kIOHIDAccessTypeGranted;
+
+        // If Input Monitoring is off (should be 'Granted' when Accessibility is checked), request it
+        if (!inputMonitoringEnabled) {
+            IOHIDRequestAccess(kIOHIDRequestTypeListenEvent);
+
+            // Do not enable clicking because the user might not have a way to stop the clicking because the FN
+            // key will not be detected and they might not have a keyboard shortcut set (which might not be detected
+            // either)
             return;
         }
-
-        if (@available(macOS 10.15, *)) {
-            BOOL inputMonitoringEnabled = IOHIDCheckAccess(kIOHIDRequestTypeListenEvent) == kIOHIDAccessTypeGranted;
-
-            // If Input Monitoring is off (should be 'Granted' when Accessibility is checked), request it
-            if (!inputMonitoringEnabled) {
-                IOHIDRequestAccess(kIOHIDRequestTypeListenEvent);
-
-                // Do not enable clicking because the user might not have a way to stop the clicking because the FN
-                // key will not be detected and they might not have a keyboard shortcut set (which might not be detected
-                // either)
-                return;
-            }
-        }
-
-        // Button
-        int selectedButton;
-        switch ([buttonSelector indexOfSelectedItem]) {
-            case 0: selectedButton = LEFT; break;
-            case 1: selectedButton = RIGHT; break;
-            case 2: selectedButton = MIDDLE; break;
-            default: selectedButton = LEFT; break;
-        }
-        
-        // Rate
-        NSInteger selectedRate = [rateSelector intValue];
-        NSInteger selectedRateUnit = ([rateUnitSelector indexOfSelectedItem]==0)?1000:60000;
-
-        double rate = selectedRateUnit / selectedRate; // a click every 'rate' (in ms)
-        
-        // Start Clicking or add the advanced preferences ?
-        if (!mode)
-            [clicker startClicking:selectedButton rate:rate startAfter:0 stopAfter:0 ifStationaryFor:0];
-        else
-        {
-            NSInteger startAfter = ([startAfterCheckbox state])?([startAfterSelector intValue]*(([startAfterUnitSelector indexOfSelectedItem]==0)?1:60)):0;
-                        
-            NSInteger stopAfter = ([stopAfterCheckbox state])?([stopAfterSelector intValue]*(([stopAfterUnitSelector indexOfSelectedItem]==0)?1:60)):0;
-                        
-            NSInteger stationaryFor = ([ifStationaryCheckbox state])?([ifStationaryForCheckbox state]?[ifStationaryForSelector intValue]:1):0;
-            
-            [clicker startClicking:selectedButton rate:rate startAfter:startAfter stopAfter:stopAfter ifStationaryFor:stationaryFor];
-        }
-        
-        [self startedClicking];
     }
+
+    // NEW: interval mode path (low-frequency single click every N minutes)
+    if (self.ac_intervalModeEnabled) {
+        [self ac_startIntervalTimer];
+        [self startedClicking]; // updates UI to "Stop"
+        return;
+    }
+
+    // Button
+    int selectedButton;
+    switch ([buttonSelector indexOfSelectedItem]) {
+        case 0: selectedButton = LEFT; break;
+        case 1: selectedButton = RIGHT; break;
+        case 2: selectedButton = MIDDLE; break;
+        default: selectedButton = LEFT; break;
+    }
+    
+    // Rate
+    NSInteger selectedRate = [rateSelector intValue];
+    NSInteger selectedRateUnit = ([rateUnitSelector indexOfSelectedItem]==0)?1000:60000;
+
+    double rate = selectedRateUnit / selectedRate; // a click every 'rate' (in ms)
+    
+    // Start Clicking or add the advanced preferences ?
+    if (!mode)
+        [clicker startClicking:selectedButton rate:rate startAfter:0 stopAfter:0 ifStationaryFor:0];
+    else
+    {
+        NSInteger startAfter = ([startAfterCheckbox state])?([startAfterSelector intValue]*(([startAfterUnitSelector indexOfSelectedItem]==0)?1:60)):0;
+                    
+        NSInteger stopAfter = ([stopAfterCheckbox state])?([stopAfterSelector intValue]*(([stopAfterUnitSelector indexOfSelectedItem]==0)?1:60)):0;
+                    
+        NSInteger stationaryFor = ([ifStationaryCheckbox state])?([ifStationaryForCheckbox state]?[ifStationaryForSelector intValue]:1):0;
+        
+        [clicker startClicking:selectedButton rate:rate startAfter:startAfter stopAfter:stopAfter ifStationaryFor:stationaryFor];
+    }
+    
+    [self startedClicking];
 }
 
 - (void)startedClicking {
@@ -358,6 +406,117 @@
     iconIndex++;
     if (iconIndex >= [iconArray count]) iconIndex = 0;
     [NSApp setApplicationIconImage:[iconArray objectAtIndex:iconIndex]];
+}
+
+// =======================
+// Interval Mode support
+// =======================
+static NSString * const kACIntervalModeEnabledKey = @"ACIntervalModeEnabled";
+static NSString * const kACIntervalSecondsKey     = @"ACIntervalSeconds";
+
+- (void)ac_updateUIForIntervalMode
+{
+    BOOL interval = self.ac_intervalModeEnabled;
+
+    // Optional UI controls if you connect them later
+    if (self.ac_intervalMinutesField) self.ac_intervalMinutesField.enabled = interval;
+    if (self.ac_intervalModeCheckbox) self.ac_intervalModeCheckbox.state = interval ? NSControlStateValueOn : NSControlStateValueOff;
+
+    // Disable CPS controls when interval mode is on (so users don’t change both)
+    if (rateSelector)      [rateSelector setEnabled:!interval];
+    if (rateUnitSelector)  [rateUnitSelector setEnabled:!interval];
+}
+
+- (IBAction)ac_toggleIntervalMode:(id)sender
+{
+    self.ac_intervalModeEnabled = (self.ac_intervalModeCheckbox.state == NSControlStateValueOn);
+    [userDefaults setBool:self.ac_intervalModeEnabled forKey:kACIntervalModeEnabledKey];
+    [self ac_updateUIForIntervalMode];
+
+    // If user disables while running, stop the timer
+    if (!self.ac_intervalModeEnabled && self.ac_intervalTimer) {
+        [self ac_stopIntervalTimer];
+        [self stoppedClicking];
+    }
+}
+
+- (IBAction)ac_intervalMinutesChanged:(id)sender
+{
+    NSInteger minutes = self.ac_intervalMinutesField.integerValue;
+    if (minutes < 1) minutes = 1;
+
+    NSInteger seconds = minutes * 60;
+    if (seconds > 24 * 60 * 60) seconds = 24 * 60 * 60; // clamp to 24h
+
+    self.ac_intervalSeconds = seconds;
+    [userDefaults setInteger:self.ac_intervalSeconds forKey:kACIntervalSecondsKey];
+
+    // If timer is running, reschedule with new interval
+    if (self.ac_intervalTimer) {
+        [self ac_startIntervalTimer];
+    }
+}
+
+- (void)ac_startIntervalTimer
+{
+    [self ac_stopIntervalTimer];
+
+    if (self.ac_intervalSeconds < 1) self.ac_intervalSeconds = 1;
+
+    dispatch_queue_t q = dispatch_get_main_queue();
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, q);
+
+    uint64_t interval_ns = (uint64_t)self.ac_intervalSeconds * (uint64_t)NSEC_PER_SEC;
+    uint64_t leeway_ns   = (uint64_t)(0.5 * (double)NSEC_PER_SEC); // allow 0.5s jitter to save CPU
+
+    // First click after one interval; change DISPATCH_TIME_NOW to 0 for immediate first click
+    dispatch_source_set_timer(timer,
+                              dispatch_time(DISPATCH_TIME_NOW, interval_ns),
+                              interval_ns,
+                              leeway_ns);
+
+    __weak typeof(self) weakSelf = self;
+    dispatch_source_set_event_handler(timer, ^{
+        [weakSelf ac_performSingleClick];
+    });
+
+    self.ac_intervalTimer = timer;
+    dispatch_resume(timer);
+}
+
+- (void)ac_stopIntervalTimer
+{
+    if (self.ac_intervalTimer) {
+        dispatch_source_cancel(self.ac_intervalTimer);
+        self.ac_intervalTimer = nil;
+    }
+}
+
+- (void)ac_performSingleClick
+{
+    // Current cursor position
+    CGEventRef event = CGEventCreate(NULL);
+    if (!event) return;
+    CGPoint p = CGEventGetLocation(event);
+    CFRelease(event);
+
+    // Respect the current button selection (0=left, 1=right, 2=middle)
+    NSInteger idx = [buttonSelector indexOfSelectedItem];
+    CGMouseButton btn = kCGMouseButtonLeft;
+    CGEventType downT = kCGEventLeftMouseDown;
+    CGEventType upT   = kCGEventLeftMouseUp;
+
+    if (idx == 1) { btn = kCGMouseButtonRight;  downT = kCGEventRightMouseDown; upT = kCGEventRightMouseUp; }
+    else if (idx == 2) { btn = kCGMouseButtonCenter; downT = kCGEventOtherMouseDown; upT = kCGEventOtherMouseUp; }
+
+    CGEventRef down = CGEventCreateMouseEvent(NULL, downT, p, btn);
+    CGEventRef up   = CGEventCreateMouseEvent(NULL, upT,   p, btn);
+    if (down && up) {
+        CGEventPost(kCGHIDEventTap, down);
+        CGEventPost(kCGHIDEventTap, up);
+    }
+    if (down) CFRelease(down);
+    if (up)   CFRelease(up);
 }
 
 @end
